7b980d5f1937d4e8f0793b94dbc8240a568795d8
Roberto Santini <santini@netresults.it>
2023-03-13 22:58:55 +0100

    APIs tuning

--- src/Entity/PostIt.php
+++ src/Entity/PostIt.php
@@ -2,38 +2,81 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\PostItRepository;
+use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PostItRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(uriTemplate: '/postIts/{id}'),
+        new Post(uriTemplate: '/postIts', denormalizationContext: ['groups' => ['postIt:write']]),
+        new Put(uriTemplate: '/postIts/{id}'),
+        new Patch(uriTemplate: '/postIts/{id}'),
+        new Delete(uriTemplate: '/postIts/{id}'),
+    ],
+    formats: ['json'],
+    normalizationContext: ['groups' => ['postIt:read']],
+    paginationEnabled: false,
+)]
 class PostIt
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[ApiProperty(example: 1)]
+    #[Groups(['postIt:read'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[ApiProperty(example: 'Complete the jQuery course at ITS Prodigi')]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 1000)]
+    #[Groups(['postIt:read', 'postIt:write'])]
     private ?string $note = null;
 
     #[ORM\Column(length: 32)]
-    private ?string $color = null;
+    #[Assert\CssColor]
+    #[Groups(['postIt:read', 'postIt:write'])]
+    private ?string $color = '#ffff00';
 
     #[ORM\Column]
-    private ?bool $isDone = null;
+    #[ApiProperty(example: 'false')]
+    #[ApiFilter(BooleanFilter::class)]
+    #[Groups(['postIt:read'])]
+    private ?bool $isDone = false;
 
     #[ORM\Column]
-    private ?int $position = null;
+    #[Assert\GreaterThanOrEqual(0)]
+    #[Groups(['postIt:read'])]
+    private ?int $position = 0;
 
     #[ORM\Column]
-    private ?\DateTimeImmutable $lastModified = null;
+    #[Groups(['postIt:read'])]
+    private ?DateTimeImmutable $lastModified;
 
     #[ORM\ManyToOne(inversedBy: 'postIts')]
     #[ORM\JoinColumn(nullable: false)]
+    #[ApiProperty(example: 'api/todoLists/1')]
+    #[Groups(['postIt:read', 'postIt:write'])]
     private ?TodoList $todoList = null;
+
+    public function __construct()
+    {
+        $this->lastModified = new DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -88,16 +131,9 @@ class PostIt
         return $this;
     }
 
-    public function getLastModified(): ?\DateTimeImmutable
+    public function getLastModified(): ?DateTimeImmutable
     {
         return $this->lastModified;
-    }
-
-    public function setLastModified(\DateTimeImmutable $lastModified): self
-    {
-        $this->lastModified = $lastModified;
-
-        return $this;
     }
 
     public function getTodoList(): ?TodoList
--- src/Entity/TodoList.php
+++ src/Entity/TodoList.php
@@ -2,33 +2,57 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Repository\TodoListRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TodoListRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(uriTemplate: '/todoLists/{id}'),
+        new GetCollection(uriTemplate: '/todoLists'),
+        new Post(uriTemplate: '/todoLists'),
+        new Patch(uriTemplate: '/todoLists/{id}'),
+        new Delete(uriTemplate: '/todoLists/{id}'),
+    ],
+    formats: ['json'],
+    paginationEnabled: false,
+)]
 class TodoList
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[ApiProperty(example: 1)]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[ApiProperty(example: 'Main TODO list')]
+    #[ApiFilter(SearchFilter::class, strategy: 'partial')]
     private ?string $title = null;
 
     #[ORM\OneToMany(mappedBy: 'todoList', targetEntity: PostIt::class, orphanRemoval: true)]
+    #[ApiProperty(example: ['/api/postIts/1', '/api/postIts/2'])]
     private Collection $postIts;
 
     #[ORM\Column]
-    private ?\DateTimeImmutable $lastModified = null;
+    private ?DateTimeImmutable $lastModified;
 
     public function __construct()
     {
         $this->postIts = new ArrayCollection();
+        $this->lastModified = new DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -56,37 +80,30 @@ class TodoList
         return $this->postIts;
     }
 
-    public function addPostIt(PostIt $postIt): self
-    {
-        if (!$this->postIts->contains($postIt)) {
-            $this->postIts->add($postIt);
-            $postIt->setTodoList($this);
-        }
+//    public function addPostIt(PostIt $postIt): self
+//    {
+//        if (!$this->postIts->contains($postIt)) {
+//            $this->postIts->add($postIt);
+//            $postIt->setTodoList($this);
+//        }
+//
+//        return $this;
+//    }
+//
+//    public function removePostIt(PostIt $postIt): self
+//    {
+//        if ($this->postIts->removeElement($postIt)) {
+//            // set the owning side to null (unless already changed)
+//            if ($postIt->getTodoList() === $this) {
+//                $postIt->setTodoList(null);
+//            }
+//        }
+//
+//        return $this;
+//    }
 
-        return $this;
-    }
-
-    public function removePostIt(PostIt $postIt): self
-    {
-        if ($this->postIts->removeElement($postIt)) {
-            // set the owning side to null (unless already changed)
-            if ($postIt->getTodoList() === $this) {
-                $postIt->setTodoList(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getLastModified(): ?\DateTimeImmutable
+    public function getLastModified(): ?DateTimeImmutable
     {
         return $this->lastModified;
-    }
-
-    public function setLastModified(\DateTimeImmutable $lastModified): self
-    {
-        $this->lastModified = $lastModified;
-
-        return $this;
     }
 }
